4f1b0590d7fc578c3ccfd205c8b6d8cf3b3bf0de
Luke Trimble <lucasftrimble@gmail.com>
2020-02-14 14:59:07 -0800

    add all inputs for form

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
+      var addressInput = $("input#address").val();
       var auditfirmInput = $("input#auditfirm").val();
       var auditorInput = $("input#auditor").val();
-      var addressInput = $("input#address").val();
+      var attorneyInput = $("input#attorney").val();
+      var clientInput = $("input#client").val();
       var date1Input = $("input#date1").val();
       var date2Input = $("input#date2").val();
       var date3Input = $("input#date3").val();
       var date4Input = $("input#date4").val();
-      var client1Input = $("input#client1").val();
+      var entityInput = $("input#entity").val();
+      var laInput = $("input#la").val();
       var shortInput = $("input#short").val();
-      var title1Input= $("input#title1").val(); 
+      var titleInput= $("input#title").val(); 
   
+      $(".address").text(addressInput);
       $(".auditfirm").text(auditfirmInput);
       $(".auditor").text(auditorInput);
-      $(".address").text(addressInput);
+      $(".attorney").text(attorneyInput);
+      $(".client").text(clientInput);
       $(".date1").text(date1Input);
       $(".date2").text(date2Input);
       $(".date3").text(date3Input);
       $(".date4").text(date4Input);
-      $(".client1").text(client1Input);
+      $(".entityInput").text(entityInput);
+      $(".laInput").text(laInput);
       $(".short").text(shortInput);
-      $(".title1").text(title1Input);
+      $(".title").text(titleInput);
       
   
       $("#auditletter").show();
